48bf84fadced9707133dab8719b30c12b21de338
leocsdev <leocsdev@gmail.com>
2023-07-04 00:37:24 +0800

    L009. Creating and activating your first WordPress theme

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WP Devs</title>
+</head>
+
+<body>
+  <h1>Hello WP Devs!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae amet aspernatur architecto repellendus! Labore culpa autem modi, ab neque rem inventore, ad laboriosam ex, dolorum quia odit. Cupiditate, debitis. Recusandae, possimus. Eaque hic voluptatibus cumque corrupti saepe tenetur perspiciatis laboriosam vel debitis at exercitationem itaque eos, autem, sit odit iusto?</p>
+</body>
+
+</html>
