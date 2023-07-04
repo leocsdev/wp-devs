86bde6d159079b82c24e33038a9409a7cf35d4e9
leocsdev <leocsdev@gmail.com>
2023-07-04 16:14:47 +0800

    L014. Enqueuing stylesheets the right way (part 3)

--- functions.php
+++ functions.php
@@ -6,5 +6,6 @@
 function wpdevs_load_scripts() {
   // wp_enqueue_style('wpdevs-style', get_stylesheet_uri(), array(), '1.0', 'all');
   wp_enqueue_style('wpdevs-style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), 'all');
+  wp_enqueue_style('wp-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null, 'all');
 }
 add_action('wp_enqueue_scripts', 'wpdevs_load_scripts');
