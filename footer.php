1024daa715c2288ea170a642e23243c44fb69076
leocsdev <leocsdev@gmail.com>
2023-07-06 22:39:36 +0800

    L019. Creating the main menu's mobile version

--- footer.php
+++ footer.php
@@ -7,7 +7,6 @@
       <?php wp_nav_menu(array('theme_location' => 'wp_devs_footer_menu', 'depth' => 1)) ?>
     </nav>
   </div>
-
 </footer>
 </div>
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -3,14 +3,12 @@
 
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
 </head>
 
-<body <?php body_class() ?>>
-  <h1>Hello WP Devs!</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae amet aspernatur architecto repellendus! Labore culpa autem modi, ab neque rem inventore, ad laboriosam ex, dolorum quia odit. Cupiditate, debitis. Recusandae, possimus. Eaque hic voluptatibus cumque corrupti saepe tenetur perspiciatis laboriosam vel debitis at exercitationem itaque eos, autem, sit odit iusto?</p>
-
+<body <?php body_class(); ?>>
   <div id="page" class="site">
     <header>
       <section class="top-bar">
@@ -26,7 +24,14 @@
       <section class="menu-area">
         <div class="container">
           <nav class="main-menu">
-            <?php wp_nav_menu(array('theme_location' => 'wp_devs_main_menu')) ?>
+            <button class="check-button">
+              <div class="menu-icon">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+              </div>
+            </button>
+            <?php wp_nav_menu(array('theme_location' => 'wp_devs_main_menu', 'depth' => 2)); ?>
           </nav>
         </div>
       </section>
